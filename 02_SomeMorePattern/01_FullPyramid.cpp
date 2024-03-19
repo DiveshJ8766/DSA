27d4daf96eaebf81403e33ae892566fa11af5f98
Divesh Jadhav <diveshvjadhav@gmail.com>
2024-03-20 02:05:15 +0530

    Added Full Pyramid Second Solution

--- 02_SomeMorePattern/01_FullPyramid.cpp
+++ 02_SomeMorePattern/01_FullPyramid.cpp
@@ -7,6 +7,8 @@ int main(){
     cout<<"Enter Number : ";
     cin>>n;
     
+     cout<<"\n\n ------------------- First Solution : ---------------\n\n";
+    
     for(int i = 0;i < n;i++){
         
         for(int j = 0;j < n;j++){
@@ -19,7 +21,22 @@ int main(){
             
         }
         cout<<endl;
+    }
+    
+    cout<<"\n\n ------------------- Second Solution : ---------------\n\n";
+    
+   for(int i = 0;i < n;i++){
         
+        for(int j = 0;j < (n-i-1);j++){
+        
+            cout<<" ";
+            
+        }
+        
+        for(int j = 0;j <= i;j++){
+            cout<<"* ";
+        }
+        cout<<endl;
     }
     
     return 0;
