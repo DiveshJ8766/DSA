395e2fa0d2dcc7a2105544185c7a384cdba78dab
Divesh Jadhav <diveshvjadhav@gmail.com>
2024-03-20 02:37:19 +0530

    Added Inverted Full Pyramid Solution

--- 02_SomeMorePattern/01_FullPyramid.cpp
+++ 02_SomeMorePattern/01_FullPyramid.cpp
@@ -1,43 +1,50 @@
 #include <iostream>
 using namespace std;
 
-int main(){
+int main()
+{
 
     int n;
-    cout<<"Enter Number : ";
-    cin>>n;
-    
-     cout<<"\n\n ------------------- First Solution : ---------------\n\n";
-    
-    for(int i = 0;i < n;i++){
-        
-        for(int j = 0;j < n;j++){
-            
-            if(j < (n-i-1)){
-                cout<<" ";
-            }else{
-                cout<<"* ";
+    cout << "Enter Number : ";
+    cin >> n;
+
+    cout << "\n\n ------------------- First Solution : ---------------\n\n";
+
+    for (int i = 0; i < n; i++)
+    {
+
+        for (int j = 0; j < n; j++)
+        {
+
+            if (j < (n - i - 1))
+            {
+                cout << " ";
             }
-            
+            else
+            {
+                cout << "* ";
+            }
         }
-        cout<<endl;
+        cout << endl;
     }
-    
-    cout<<"\n\n ------------------- Second Solution : ---------------\n\n";
-    
-   for(int i = 0;i < n;i++){
-        
-        for(int j = 0;j < (n-i-1);j++){
-        
-            cout<<" ";
-            
+
+    cout << "\n\n ------------------- Second Solution : ---------------\n\n";
+
+    for (int i = 0; i < n; i++)
+    {
+
+        for (int j = 0; j < (n - i - 1); j++)
+        {
+
+            cout << " ";
         }
-        
-        for(int j = 0;j <= i;j++){
-            cout<<"* ";
+
+        for (int j = 0; j <= i; j++)
+        {
+            cout << "* ";
         }
-        cout<<endl;
+        cout << endl;
     }
-    
+
     return 0;
 }
---
+++ 02_SomeMorePattern/02_InvertedFullPyramid.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+
+    int n;
+    cout<<"Enter Number : ";
+    cin>>n;
+    
+     cout<<"\n\n ------------------- First Solution : ---------------\n\n";
+    
+    for(int i = 0;i < n;i++){
+        
+        for(int j = 0;j < n;j++){
+            
+            if(j < i){
+                cout<<" ";
+            }else{
+                cout<<"* ";
+            }
+            
+        }
+        cout<<endl;
+    }
+    
+    cout<<"\n\n ------------------- Second Solution : ---------------\n\n";
+    
+   for(int i = 0;i < n;i++){
+        
+        for(int j = 0;j < i;j++){
+        
+            cout<<" ";
+            
+        }
+        
+        for(int j = 0;j < n - i;j++){
+            cout<<"* ";
+        }
+        cout<<endl;
+    }
+    
+    return 0;
+}
